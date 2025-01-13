58c45a33dcfd731c76aff61a7c60d4fb8f4027dd
gonos <gonosanna@mail.ru>
2025-01-14 02:05:01 +0300

    feat: добавила слайдер, футер, хедер

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.slider__button--next',
+        prevEl: '.slider__button--prev',
+    },
+
+    // autoplay: {
+    //     delay: 3500,
+    // },
+
+    simulateTouch: true,
+    touchRatio: 2,
+    touchAngle: 40,
+    grabCursor: true,
+
+    slideToClickedSlide: true,
+
+    hashNavigation: {
+        watchState: true,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    // autoHeight: true,
+    slidesPerView: 1,
+});
